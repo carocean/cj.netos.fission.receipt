6d549f6e2fe91920ce4ab3739a8d153f217a6606
cj <cj@ss.com>
2021-03-19 01:38:34 +0800

    1.0.0

--- cj.netos.fission.openports/src/cj/netos/fission/ports/ICashierReceiptPorts.java
+++ cj.netos.fission.openports/src/cj/netos/fission/ports/ICashierReceiptPorts.java
@@ -17,6 +17,16 @@ public interface ICashierReceiptPorts extends IOpenportService {
 
     @CjOpenport(usage = "抢包,返回抢单号")
     String snatchEnvelope(ISecuritySession session,
-                        @CjOpenportParameter(usage = "被抢对象", name = "person") String person
+                          @CjOpenportParameter(usage = "被抢对象", name = "person") String person
+    ) throws CircuitException;
+
+    @CjOpenport(usage = "营业出款")
+    void cashierOutBusiness(ISecuritySession session,
+                            @CjOpenportParameter(usage = "出款金额", name = "amount") long amount
+    ) throws CircuitException;
+
+    @CjOpenport(usage = "收益出款")
+    void cashierOutIncome(ISecuritySession session,
+                          @CjOpenportParameter(usage = "出款金额", name = "amount") long amount
     ) throws CircuitException;
 }
--- cj.netos.fission.program/src/cj/netos/fission/ports/CashierReceiptPorts.java
+++ cj.netos.fission.program/src/cj/netos/fission/ports/CashierReceiptPorts.java
@@ -17,10 +17,13 @@ import java.util.UUID;
 public class CashierReceiptPorts implements ICashierReceiptPorts {
     @CjServiceRef(refByName = "@.rabbitmq.producer.fission-mf")
     IRabbitMQProducer rabbitMQProducer;
+    @CjServiceRef(refByName = "@.rabbitmq.producer.fission-account")
+    IRabbitMQProducer rabbitMQProducerAccount;
     @CjServiceRef
     ICashierService cashierService;
     @Override
     public void withdraw(ISecuritySession session, long amount) throws CircuitException {
+//        throw new CircuitException("800",String.format("最近由于系统升级，暂时停止提现服务。暂定于本月22号开通服务，当听到手机地微app有他人抢红包的提示音后，表示该服务已开通。"));
 //        CJSystem.logging().info(getClass(),String.format("%s %s欲提现%s分",session.property("nickName"),session.principal(),amount));
         cashierService.checkWithdrawCondition(session.principal(),amount);
         AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
@@ -50,5 +53,33 @@ public class CashierReceiptPorts implements ICashierReceiptPorts {
                 .build();
         rabbitMQProducer.publish("fission.mf", properties, new byte[0]);
         return sn;
+    }
+
+    @Override
+    public void cashierOutBusiness(ISecuritySession session, long amount) throws CircuitException {
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+                .type("/cashier/account.mhub")
+                .headers(new HashMap<String, Object>() {{
+                    put("command", "cashierOutBusiness");
+                    put("person", session.principal());
+                    put("nick-name", session.property("nickName"));
+                    put("amount",amount);
+                }})
+                .build();
+        rabbitMQProducerAccount.publish("fission.mf", properties, new byte[0]);
+    }
+
+    @Override
+    public void cashierOutIncome(ISecuritySession session, long amount) throws CircuitException {
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+                .type("/cashier/account.mhub")
+                .headers(new HashMap<String, Object>() {{
+                    put("command", "cashierOutIncome");
+                    put("person", session.principal());
+                    put("nick-name", session.property("nickName"));
+                    put("amount",amount);
+                }})
+                .build();
+        rabbitMQProducerAccount.publish("fission.mf", properties, new byte[0]);
     }
 }
